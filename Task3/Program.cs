8d8201120c2ad01eac78310f49a97eb549d28222
Sahib Novruzov <sahib.novruzov1996@outlook.com>
2023-04-29 09:49:09 +0400

    Add project files.

---
+++ Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int a, b, c,d;
+        a = 5;
+        b = 3;
+        c = 45;
+        d = a+b+c;
+        Console.WriteLine(d);
+        Console.ReadLine();
+    }
+}
